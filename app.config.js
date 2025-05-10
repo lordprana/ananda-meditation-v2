b016dc48f3c00d4366a730074dcbaa87da118258
Matthew Gaba <matthew@sully.ai>
2025-05-10 12:14:21 -0700

    Configure project for eas builds

--- app.config.js
+++ app.config.js
@@ -1,8 +1,23 @@
-import 'dotenv/config';
+import 'dotenv/config'
 
-export default ({ config }) => ({
-  ...config,
-  extra: {
-    CONTENTFUL_CACHE_SERVER_PATH: process.env.CONTENTFUL_CACHE_SERVER_PATH,
+const config = {
+  'name': 'ananda-meditation-v2',
+  'slug': 'ananda-meditation-v2',
+  'version': '1.0.0',
+  'sdkVersion': '53.0.0',
+  'platforms': [
+    'ios',
+    'android',
+  ],
+  'ios': {
+    'bundleIdentifier': 'com.mukti.devclient',
   },
-});
+  'extra': {
+    'CONTENTFUL_CACHE_SERVER_PATH': process.env.CONTENTFUL_CACHE_SERVER_PATH,
+    'eas': {
+      'projectId': '94268b94-4014-4cab-b496-ec235086f763',
+    },
+  },
+}
+
+export default config
